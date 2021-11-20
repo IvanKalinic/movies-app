9d948d21243ba64c8e38fd92144fab79fd9c48fa
Ivan Kalinić <ivankalinic1998@gmail.com>
2021-11-20 12:07:42 +0100

    Added fetch for tranding movies

---
+++ client/src/apis/index.js
@@ -0,0 +1,9 @@
+const BASE_MOVIES_URL = "https://api.themoviedb.org/3/";
+const { REACT_APP_MOVIEDB_API_KEY } = process.env;
+
+export const fetchTrandingMovies = async () => {
+  const response = await fetch(
+    BASE_MOVIES_URL + `trending/all/day?api_key=${REACT_APP_MOVIEDB_API_KEY}`
+  );
+  return response.json();
+};
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { fetchTrandingMovies } from "../../apis";
 
 const Home = () => {
+  useEffect(() => {
+    fetchTrandingMovies().then((res) => console.log(res.results));
+  }, []);
+
   return <div>Home page</div>;
 };
 
