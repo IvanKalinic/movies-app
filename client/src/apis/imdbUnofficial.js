fea742091120719a0ccc34c186f571915038b22e
t.kurtovic <tkurtovic98@gmail.com>
2022-01-08 01:32:41 +0100

    Add display of fetched recommended movies

--- client/src/apis/imdbUnofficial.js
+++ client/src/apis/imdbUnofficial.js
@@ -1,16 +1,29 @@
 import axios from "axios";
 
+const { REACT_APP_SERVER_BASE_URL } = process.env;
+
 var options = {
-    method: 'GET',
-    url: 'https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/',
-    headers: {
-        'x-rapidapi-host': 'imdb-internet-movie-database-unofficial.p.rapidapi.com',
-        'x-rapidapi-key': 'de19b552d2mshf72d708fe2d4889p172e0ejsn2263d7aad5f6'
-    }
+  method: "GET",
+  url: "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/",
+  headers: {
+    "x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
+    "x-rapidapi-key": "de19b552d2mshf72d708fe2d4889p172e0ejsn2263d7aad5f6",
+  },
 };
 
 export const callImdbUnofficial = async (search) => {
-    const optionWithSearchedText = { ...options, url: options.url + search};
-    const response = await axios.request(optionWithSearchedText);
-    return response;
-}
+  const optionWithSearchedText = { ...options, url: options.url + search };
+  const response = await axios.request(optionWithSearchedText);
+  return response;
+};
+
+export const fetchMovie = async (name) => {
+  try {
+    return await axios.get(
+      REACT_APP_SERVER_BASE_URL + "/imdbUnofficial/findByTitle?title=" + name
+    );
+  } catch (err) {
+    callImdbUnofficial(name);
+    return err;
+  }
+};
--- client/src/components/MovieDetails/MovieNameRecommendation.js
+++ client/src/components/MovieDetails/MovieNameRecommendation.js
@@ -1,20 +1,37 @@
 import React, { useState, useEffect } from "react";
+import { fetchMovie } from "../../apis/imdbUnofficial";
 import { fetchRecommendationByName } from "../../apis/recommendation";
+import { ReactstrapCard } from "../../components/ReactstrapCards/Card";
+import { CardGroup } from "reactstrap";
 
 export const MovieNameRecommendation = ({ movieName }) => {
-  const [recommendedMovies, setRecommendedMovies] = useState(null);
+  const [recommendedMovies, setRecommendedMovies] = useState([]);
 
   useEffect(() => {
-    fetchRecommendationByName(movieName).then((movies) => {
-      setRecommendedMovies(movies);
+    fetchRecommendationByName(movieName).then(async (recommendedMovieNames) => {
+      let recMovies = [];
+      for (let i = 0; i < recommendedMovieNames.length; i++) {
+        const res = await fetchMovie(recommendedMovieNames[i]);
+        recMovies.push(res.data);
+      }
+      console.log(recMovies);
+      setRecommendedMovies(recMovies);
     });
   }, []);
 
   return (
     <div>
       {recommendedMovies &&
-        recommendedMovies.map((movie) => {
-          return <p>{movie}</p>;
+        recommendedMovies.map((movie, index) => {
+          return (
+            <CardGroup>
+              <ReactstrapCard
+                id={index}
+                title={movie?.title}
+                picture={movie?.image}
+              />
+            </CardGroup>
+          );
         })}
     </div>
   );
