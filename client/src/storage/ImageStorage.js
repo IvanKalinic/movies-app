91fc822613d0d72af46dd31896b570b32ebfd183
t.kurtovic <tkurtovic98@gmail.com>
2022-01-09 02:24:27 +0100

    Refactor search page ui. Refactor Movie List ui.

--- client/src/storage/ImageStorage.js
+++ client/src/storage/ImageStorage.js
@@ -1,11 +1,13 @@
 import Google from "../assets/img/google.png";
 import Github from "../assets/img/github.png";
 import Facebook from "../assets/img/facebook.png";
+import SearchWallpaper from "../assets/img/netflix-image.jpg";
 
 const imageStorage = {
-    google: Google,
-    github: Github,
-    facebook: Facebook
-}
+  google: Google,
+  github: Github,
+  facebook: Facebook,
+  searchBackground: SearchWallpaper,
+};
 
 export default imageStorage;
